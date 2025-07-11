309a18e46c1da9ceca9e408510b88b972cc89c52
desivar <jillian_desire@yahoo.es>
2025-07-10 22:11:38 -0600

    nextconfig.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// frontend/next.config.js
+   module.exports = {
+     reactStrictMode: true,
+     cssModules: true,
+     cssLoaderOptions: {
+       importLoaders: 1,
+       localIdentName: '[local]__[hash:base64:5]',
+     },
+   };
