70dbf1ffa4b7d753b9fe3c0347c73c607f1e2bfc
desivar <jillian_desire@yahoo.es>
2025-07-10 21:41:35 -0600

    onfig/db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,15 @@
-// server/config/db.js
-const mongoose = require('mongoose');
+     // server/config/db.js
+     const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error('MongoDB Connection Error:', err);
-    process.exit(1);
-  }
-};
+     const connectDB = async () => {
+       try {
+         await mongoose.connect(process.env.MONGODB_URI);
+         console.log('MongoDB Connected');
+       } catch (err) {
+         console.error('MongoDB Connection Error:', err);
+         process.exit(1);
+       }
+     };
 
-module.exports = connectDB;
+     module.exports = connectDB;
+     
